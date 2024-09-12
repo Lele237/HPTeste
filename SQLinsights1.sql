05bda82ea86136b19d937c5031325685749f0eb0
ApisLock <108353593+Lele237@users.noreply.github.com>
2024-09-12 15:04:12 -0300

    Update SQLinsights1.sql

--- SQLinsights1.sql
+++ SQLinsights1.sql
@@ -1,16 +1,16 @@
-  -- Análise de performance por canal e campanha
+  -- AnÃ¡lise de performance por canal e campanha
 SELECT 
     canal, 
     campanha, 
-    SUM(impressões) AS total_impressões, 
+    SUM(impressÃµes) AS total_impressÃµes, 
     SUM(cliques) AS total_cliques, 
     SUM(leads) AS total_leads, 
     SUM(valor_gasto) AS total_gasto,
-    (SUM(cliques) / SUM(impressões)) * 100 AS ctr,  -- Taxa de Cliques
-    (SUM(leads) / SUM(cliques)) * 100 AS taxa_conversao,  -- Taxa de Conversão
+    (SUM(cliques) / SUM(impressÃµes)) * 100 AS ctr,  -- Taxa de Cliques
+    (SUM(leads) / SUM(cliques)) * 100 AS taxa_conversao,  -- Taxa de ConversÃ£o
     (SUM(valor_gasto) / NULLIF(SUM(leads), 0)) AS cpl  -- Custo por Lead
 FROM 
-    [Cópia de [HP]] PLANILHA TESTE TÉCNICO (1) - Copiar (3) - extract]
+    [CÃ³pia de [HP]] PLANILHA TESTE TÃ‰CNICO (1) - Copiar (3) - extract]
 WHERE 
     data >= '2024-08-01' AND data <= '2024-08-31'
 GROUP BY 
@@ -19,35 +19,35 @@ ORDER BY
     taxa_conversao DESC, ctr DESC;
 
 
-	--Identificação de Anúncios que têm alta despesa, mas baixa conversão.
+	--IdentificaÃ§Ã£o de AnÃºncios que tÃªm alta despesa, mas baixa conversÃ£o.
 SELECT 
-    grupo_de_anúncio, 
+    grupo_de_anÃºncio, 
     SUM(cliques) AS total_cliques, 
     SUM(leads) AS total_leads, 
     SUM(valor_gasto) AS total_gasto,
     (SUM(valor_gasto) / NULLIF(SUM(leads), 0)) AS cpl
 FROM 
-    [Cópia de [HP]] PLANILHA TESTE TÉCNICO - extract (1)]
+    [CÃ³pia de [HP]] PLANILHA TESTE TÃ‰CNICO - extract (1)]
 WHERE 
    data >= '2024-08-01' AND data <= '2024-08-31'
 GROUP BY 
-    grupo_de_anúncio
+    grupo_de_anÃºncio
 HAVING 
     SUM(leads) = 0 OR (SUM(valor_gasto) / NULLIF(SUM(leads), 0)) > 50  -- Ajuste o valor conforme a meta de CPL desejada
 ORDER BY 
     cpl DESC;
 
-	--Análise temporal para Otimização de investimentos
+	--AnÃ¡lise temporal para OtimizaÃ§Ã£o de investimentos
 SELECT 
     data, 
-    SUM(impressões) AS total_impressões, 
+    SUM(impressÃµes) AS total_impressÃµes, 
     SUM(cliques) AS total_cliques, 
     SUM(leads) AS total_leads, 
     SUM(valor_gasto) AS total_gasto,
-    (SUM(cliques) / SUM(impressões)) * 100 AS ctr, 
+    (SUM(cliques) / SUM(impressÃµes)) * 100 AS ctr, 
     (SUM(leads) / SUM(cliques)) * 100 AS taxa_conversao
 FROM 
-   [Cópia de [HP]] PLANILHA TESTE TÉCNICO - extract (1)]
+   [CÃ³pia de [HP]] PLANILHA TESTE TÃ‰CNICO - extract (1)]
 WHERE 
     data >= '2024-08-01' AND data <= '2024-08-31'
 GROUP BY 
@@ -55,14 +55,14 @@ GROUP BY
 ORDER BY 
     taxa_conversao DESC;
 
-	-- Comparação do desempenho de canais para melhor alocação de orçamento
+	-- ComparaÃ§Ã£o do desempenho de canais para melhor alocaÃ§Ã£o de orÃ§amento
 SELECT 
     canal, 
     SUM(valor_gasto) AS total_gasto, 
     SUM(leads) AS total_leads,
     (SUM(leads) / NULLIF(SUM(valor_gasto), 0)) AS leads_por_gasto
 FROM 
-    [Cópia de [HP]] PLANILHA TESTE TÉCNICO (1) - Copiar (3) - extract]
+    [CÃ³pia de [HP]] PLANILHA TESTE TÃ‰CNICO (1) - Copiar (3) - extract]
 WHERE 
     data >= '2024-08-01' AND data <= '2024-08-31'
 GROUP BY 
